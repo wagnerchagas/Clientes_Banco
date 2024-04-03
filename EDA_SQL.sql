8363a8b001a6b4500379b0c6c8a6a3fca3efffc5
wrpereira <wrp.engcivil@hotmail.com>
2024-04-02 22:02:28 -0300

    Melhorando a EDA

--- EDA_SQL.sql
+++ EDA_SQL.sql
@@ -5,6 +5,11 @@ FROM clientes
 SELECT DISTINCT profissao
 FROM clientes
 
+//*Qtde. de clientes por profissão*//
+SELECT DISTINCT count (id_cliente) as 'Qtd. de Clientes', profissao
+FROM clientes
+group by profissao
+
 //*Média de idade dos cliente por profissão *//
 
 SELECT AVG(CAST(idade as numeric)) as 'Média de Idade' , profissao
@@ -26,17 +31,32 @@ ORDER BY 'Média de Salario Mensal' desc
 
 //*Maiores salários dos clientes por profissão*//
 
-SELECT TOP (15) profissao , salario_anual
-FROM clientes
-GROUP BY profissao , salario_anual
-ORDER BY salario_anual desc
-
 //*Menores salários dos clientes por profissão*//
 
 SELECT TOP (15) profissao , salario_anual
 FROM clientes
 GROUP BY profissao , salario_anual
 ORDER BY salario_anual asc
+
+//* faixa de salários *//
+
+SELECT DISTINCT profissao,
+	CASE
+		WHEN cast(salario_anual as decimal) BETWEEN 0 AND 40000.00 THEN 'ate 40k'
+		WHEN cast(salario_anual as decimal) BETWEEN 40000.01 AND 80000.00 THEN '40K ate 80k'
+		WHEN cast(salario_anual as decimal) BETWEEN 80000.01 AND 120000.00 THEN '80K ate 120k'
+		WHEN cast(salario_anual as decimal) BETWEEN 120000.01 AND 160000.00 THEN '120K ate 160k'
+		ELSE 'ACIMA DE 160K'
+	END as 'Faixa de salario'
+FROM clientes
+ORDER BY profissao asc
+
+//* % de salários por profissão   *///
+
+SELECT cast(count(p.salario_anual) as numeric(10,3)) as 'Qtd. de Clientes', p.profissao,
+cast((count (p.salario_anual) / tmp.total)*100 as numeric(10,3)) as '% de salários por profissão'
+from clientes p , (SELECT cast(count (salario_anual) as numeric(10,3)) total from clientes ) tmp
+group by p.profissao , tmp.total
 
 //*Qtde. de contas e cartões por profissão*//
 
@@ -49,3 +69,11 @@ SELECT DISTINCT profissao , SUM (CAST (num_cartoes as numeric)) as 'Qtde de Cart
 FROM clientes
 GROUP BY  profissao
 ORDER BY 'Qtde de Cartões por Profissão' desc
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Vendas_Varejo
+
+
  
